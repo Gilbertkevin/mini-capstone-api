d28ac1e83a8da143ce113bf5722a4c473a53cb5b
Kevin Gilbert <kgilbert.og@gmail.com>
2021-10-12 21:35:00 -0500

    add requests and routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,12 @@
 class ProductsController < ApplicationController
   def one_product
-    product = 
+    product = Product.find_by(id: 1)
+    product.name
+    render json: product.as_json
+  end
+
+  def all_products
+    products = Product.all
+    render json: products.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/first_product", controller: "products", action: "one_product"
+
+  get "/all_products", controller: "products", action: "all_products"
 end
