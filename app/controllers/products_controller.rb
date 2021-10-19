b1fc326fc9e5d5421cb7eebaced1e66e5df985de
Kevin Gilbert <kgilbert.og@gmail.com>
2021-10-18 22:08:17 -0500

    added model methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,41 @@
 class ProductsController < ApplicationController
-  def one_product
-    product = Product.find_by(id: 1)
-    product.name
+  def index
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def show
+    the_id = params[:id]
+    product = Product.find_by(id: the_id)
+
+    render json: product.as_json(methods: [:is_discounted?, :tax, :total])
+  end
+
+  def create
+    product = Product.new(
+      name: params[:input_name],
+      price: params[:input_price],
+      image_url: params[:input_url],
+      description: params[:input_description],
+    )
+    product.save
     render json: product.as_json
   end
 
-  def all_products
-    products = Product.all
-    render json: products.as_json
+  def update
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+
+    render json: { message: "Product is removed" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/first_product", controller: "products", action: "one_product"
+  # all products
+  get "/products" => "products#index"
 
-  get "/all_products", controller: "products", action: "all_products"
+  # show single product
+  get "/products/:id/" => "products#show"
+
+  # create product
+  post "/products" => "products#create"
+
+  #update product
+  patch "/products/:id" => "products#update"
+
+  #delete recipe
+  delete "/products/:id" => "products#destroy"
 end
